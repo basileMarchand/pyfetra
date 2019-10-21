4e99157f50f29d7dfbe80394e0f0e5a05b38ced0
Basile MARCHAND <basile.marchand@mines-paristech.fr>
2019-10-21 13:47:27 +0200

    Use numba jit to speed-up some low level functions det and inv

--- pyfetra/tools/mathUtils.py
+++ pyfetra/tools/mathUtils.py
@@ -19,15 +19,18 @@
 
 
 import numpy as np
+from numba import jit, float64
 
+@jit
 def myDet3(m):
     m_det = m[0][0]*(m[1,1]*m[2,2]-m[1,2]*m[2,1])+m[0][1]*(m[1,2]*m[2,0]-m[1,0]*m[2,2])+m[0][2]*(m[1,0]*m[2,1]-m[1,1]*m[2,0])
     return m_det
 
+@jit
 def myDet2(m):
     return m[0][0]*m[1,1]-m[1,0]*m[0,1]
 
-
+@jit
 def myInv3(m):
     m_det = myDet3(m)
     m_inv = (1./m_det) * np.array([[m[1,1]*m[2,2]-m[1,2]*m[2,1] , m[0,2]*m[2,1]-m[0,1]*m[2,2] , m[0,1]*m[1,2]-m[0,2]*m[1,1] ],
@@ -35,7 +38,7 @@ def myInv3(m):
                                    [m[1,0]*m[2,1]-m[1,1]*m[2,0] , m[0,1]*m[2,0]-m[0,0]*m[2,1] , m[0,0]*m[1,1]-m[0,1]*m[1,0] ]])
     return m_inv
 
-
+@jit
 def myInv2(m):
     m_det = myDet2(m)
     m_inv = (1./m_det) * np.array([[ m[1,1],-m[0,1]],[-m[1,0],m[0,0] ]])
